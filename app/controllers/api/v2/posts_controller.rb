13e7f704474d6355aaa74638d3e5866d56732a2d
anton <lascivolin@gmail.com>
2016-09-25 17:31:48 +0300

    add serealize and paginate

--- app/controllers/api/v2/posts_controller.rb
+++ app/controllers/api/v2/posts_controller.rb
@@ -1,32 +1,35 @@
 module Api
-  class PostsController < ApplicationController
-
-    before_action :authenticate_user
+  class V2::PostsController < ApplicationController
 
     def index
-      render json: Post.all.to_json
+      if params[:page]
+        post = Post.page(params[:page][:number]).per(params[:page][:size])
+      else
+        post = Post.all
+      end
+      render json: post
     end
 
     def show
       post = Post.find(params[:id])
-      render json: post.to_json
+      render json: post
     end
 
     def create
       post = Post.new(post_params)
       if post.save
-        render json: post.to_json
+        render json: post
       else
-        render json: post.errors.to_json
+        render json: post.errors
       end
     end
 
     def update
       post = Post.find(params[:id])
       if post.update(post_params)
-        render json: post.to_json
+        render json: post
       else
-        render json: post.errors.to_json
+        render json: post.errors
       end
     end
 
