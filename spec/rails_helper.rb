c4286de8c6bc263acf13b378cb80094eab9cc8a1
Anton <lascivolin@gmail.com>
2016-09-28 12:37:32 +0300

    add rubocop.yml

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'database_cleaner'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -35,12 +36,10 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
   end
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
--- spec/requests/api/v2/posts_spec.rb
+++ spec/requests/api/v2/posts_spec.rb
@@ -19,7 +19,7 @@ describe 'Post' do
     end
 
     it 'sort created_at DESC' do
-      10.times { create(:post) }
+      10.times { FactoryGirl.create(:post) }
       post = Post.order('created_at DESC').first
       get_params = { sort: '-created_at' }
       get '/api/v2/posts', headers: header, params: get_params
@@ -27,16 +27,16 @@ describe 'Post' do
       expect(json['data'][0]['attributes']['title']).to eq(post.title)
     end
 
-    it 'search is working' do
-      20.times { create(:post) }
-      get_params = { search: '15' }
+    it 'search is working', :focus => true do
+      posts = 20.times { FactoryGirl.create(:post) }
+      get_params = { search: '26' }
       get '/api/v2/posts', headers: header, params: get_params
       json = JSON.parse(response.body)
-      expect(json['data'][0]['attributes']['title']).to eq('Hot news 15')
+      expect(json['data'][0]['attributes']['title']).to eq('Hot news 26')
     end
 
     it 'paginate' do
-      15.times { create(:post) }
+      posts = 15.times { create(:post) }
       get_params = { page: { number: 2 } }
       get '/api/v2/posts', headers: header, params: get_params
       json = JSON.parse(response.body)
